14439c48826ea4ee5aacdc80be2f1af519c12f1c
NitinUdasin <nitin.udasin@gmail.com>
2022-01-07 14:16:21 +0530

    V4

--- Welcome/index.js
+++ Welcome/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". Welcome to azure function test application V3."
+        ? "Hello, " + name + ". Welcome to azure function test application <b>V4</b>."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
